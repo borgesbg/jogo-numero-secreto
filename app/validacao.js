6727c7a8685d6bf8a39e53cfdf982b39d18929ba
Vinicius Borges <96083426+borgesbg@users.noreply.github.com>
2022-06-29 20:13:33 -0300

    Atualizar tentativa validacoes

--- app/validacao.js
+++ app/validacao.js
@@ -3,26 +3,45 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido<div>';
+        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        tentativas ++
         return
     }
 
     if (numeroForMaiorOuMenorQueONumeroPermitido(numero)) {
         elementoChute.innerHTML += `<div>Valor inválido, o número precisa estar entre ${menorValor} e ${maiorValor}<div>`;
+        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        tentativas ++
         return
 
     }
 
     if (numero === numeroSecreto) {
-        document.body.innerHTML = `
+        if(tentativas >= 10){
+            document.body.innerHTML =  `
             <h2>Você Acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
             <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
-
-        `
+            <div class="num-tentativas">Número de tentativas ${tentativas}</div>
+            `
+        }else{
+            document.body.innerHTML =  `
+            
+            <h2>Você Acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+            <div class="num-tentativas">Você tem sorte! Número de tentativas ${tentativas}</div>
+            `
+        }
+        
     } else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`;
+        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        tentativas ++
     } else {
-        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`;
+        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        tentativas ++
     }
 
 }
