b3dad6a38673221cab78cbe0b2822976d7b467bb
Vinicius Borges <96083426+borgesbg@users.noreply.github.com>
2022-06-30 11:09:11 -0300

    Tentativas => chutes

--- app/validacao.js
+++ app/validacao.js
@@ -3,14 +3,14 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido<div>';
-        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        elementoChute.innerHTML += `<div>Chutes: ${tentativas}</div>`;
         tentativas ++
         return
     }
 
     if (numeroForMaiorOuMenorQueONumeroPermitido(numero)) {
         elementoChute.innerHTML += `<div>Valor inválido, o número precisa estar entre ${menorValor} e ${maiorValor}<div>`;
-        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        elementoChute.innerHTML += `<div>Chutes: ${tentativas}</div>`;
         tentativas ++
         return
 
@@ -22,7 +22,7 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             <h2>Você Acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
             <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
-            <div class="num-tentativas">Número de tentativas ${tentativas}</div>
+            <div class="num-tentativas">Número de chutes utilizados: ${tentativas}</div>
             `
         }else{
             document.body.innerHTML =  `
@@ -30,17 +30,17 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             <h2>Você Acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
             <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
-            <div class="num-tentativas">Você tem sorte! Número de tentativas ${tentativas}</div>
+            <div class="num-tentativas">Você tem sorte! Com apenas ${tentativas} chutes, você acertou o número!</div>
             `
         }
         
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`;
-        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        elementoChute.innerHTML += `<div>Chutes: ${tentativas}</div>`;
         tentativas ++
     } else {
         elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`;
-        elementoChute.innerHTML += `<div>Número de tentativas ${tentativas}</div>`;
+        elementoChute.innerHTML += `<div>Chutes: ${tentativas}</div>`;
         tentativas ++
     }
 
